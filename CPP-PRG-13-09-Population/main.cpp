c48001f6ec6157d2905cbde8267797ed56db8a69
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-21 13:59:04 -0500

    Prompt written.

--- CPP-PRG-13-09-Population/main.cpp
+++ CPP-PRG-13-09-Population/main.cpp
@@ -5,11 +5,27 @@
 //  Created by Keith Smith on 11/21/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  In a population, the birth rate and death rate are calculated as follows:
+//
+//  Birth Rate = Number of Births / Population
+//  Death Rate = Number of Deaths / Population
+//
+//  For example, in a population of 100,000 that has 8,000 births and 6,000 deaths per
+//  year, the birth rate and death rate are:
+//
+//  Birth Rate = 8,000 / 100,000 = 0.08
+//  Death Rate = 6,000 / 100,000 = 0.06
+//
+//  Design a Population class that stores a population, number of births, and number of
+//  deaths for a period of time. Member functions should return the birth rate and death
+//  rate. Implement the class in a program.
+//
+//  Input Validation: Do not accept population figures less than 1, or birth or death numbers
+//  less than 0.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
     return 0;
 }
